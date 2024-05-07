808ef6d543d0e68e2525588834d94b44b3ad6137
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-05-06 21:54:49 -0300

    Update tabela_agencia.sql

--- tabela_agencia.sql
+++ tabela_agencia.sql
@@ -46,10 +46,42 @@ DELETE FROM Cliente Where id = 1;
 /* Fazendo Update na tabela cliente e agencia mundando nome e numero */
 UPDATE Cliente set nome = 'Huandres' where id = 3;
 UPDATE Agencia set numero = 641 where id = 1;
+UPDATE Cliente set id = 3 where id = 5;
 		
-		
+/* Criando tabela conta corrente que irá referenciar o id da agencia e do cliente*/
+CREATE TABLE ContaCorrente (
+	id SERIAL PRIMARY KEY,
+	numero_conta INT NOT NULL,
+	saldo DECIMAL(12, 2) NOT NULL,
+	agencia_id INT,
+	cliente_id INT,
+	
+	FOREIGN KEY (agencia_id) REFERENCES Agencia(id),
+	FOREIGN KEY (cliente_id) REFERENCES Cliente(id)
+);
+
+INSERT INTO ContaCorrente (numero_conta, saldo, agencia_id, cliente_id)
+VALUES	(1001, 5000.00, 3, 2);
+
+SELECT nome, saldo from ContaCorrente
+JOIN cliente on ContaCorrente.cliente_id = cliente.id;
+
+CREATE TABLE Movimentacoes (
+	id SERIAL PRIMARY KEY,
+	tipo varchar (12),
+	valor DECIMAL(12, 2),
+	horaeData varchar(68),
+	movimentacao_id INT NOT NULL,
+	
+	FOREIGN KEY (movimentacao_id) REFERENCES ContaCorrente(id)
+);
+
+INSERT INTO Movimentacoes (tipo, valor, horaeData, movimentacao_id)
+VALUES
+		('PIX', 350.00, '06/05/2024 21 50', 1);
+
+select * from Movimentacoes
 
 
-
-
+						  
 
